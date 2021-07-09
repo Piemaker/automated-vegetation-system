92043bf734137caf063044d7a3b5e903cb6ed141
Piemaker1 <orhman@hotmail.com>
2021-07-09 19:42:43 +0000

    Statistics page: Minimum date now shows the oldest date in the temperature database and the maximum date shows today's date, also added a paragraph that shows the access keys.

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
-
+//import models 
+const { Temperature } = require('../models/Base')
+//return page and set title & minimum date of the form to the oldest record in the database and the maximum date to the current date
 app.get('/', (req, res) => {
-  res.render("statistics",{title:"Statistics Page"})
+  Temperature.find({})
+   .sort({ "date": +1 })
+    .select("date -_id")
+    .limit(1)
+    .then(data => {
+      const currentDate = new Date();
+      res.render("statistics",{title:"Statistics Page", minDate : `${data[0].date.toISOString().split('T')[0]}`, maxDate : currentDate.toISOString().split('T')[0] })
+    })
+    .catch(err => res.send(err.message))
+  
 });
 
 module.exports = app
+
+// db.collection.find().sort({age:-1}).limit(1) // for MAX
+// db.collection.find().sort({age:+1}).limit(1) // for MIN
